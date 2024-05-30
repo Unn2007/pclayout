96e7460ab973df526a6bb164f399da09feec3167
Unn2007 <unn2007@i.ua>
2024-05-30 13:10:52 +0300

    refg_again

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const contens = document.querySelectorAll(".program-line__content");
+const allDescr=document.querySelectorAll(".program-line__descr");
+
+contens.forEach((element)=>{
+    const title=element.querySelector(".program-line__title");
+    const descr = element.querySelector(".program-line__descr");
+    // console.log()
+    
+   
+    title.addEventListener("click",(event)=>{
+        const numberClickedDescr = event.target.textContent.split("")[8]
+        descr.classList.toggle("active")
+        allDescr.forEach((el,index)=>{
+            
+            if ((el.classList.contains("active"))&&((index+1)!==(+numberClickedDescr))) {
+                el.classList.remove("active")
+               
+            }
+            
+        })
+    });
+})
